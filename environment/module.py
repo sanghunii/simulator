e40b4a073b6529b81bded5f5850716937e0d85eb
박상훈 <sanghoon20020403@gmail.com>
2025-01-16 14:41:17 +0900

    complete module: Job, Machine / remaind: Env(simulatior)

--- environment/module.py
+++ environment/module.py
@@ -16,15 +16,22 @@ class Job():
         self.operatingNum = operatingNum
         self.inspected = inspected
         self.leadTimes = []
+    ##operating갯수만큼 leadTime생성 
         for i in range(0, operatingNum):
             self.leadTimes.append(randint(10,30))
         
     
+##Test Method => Job의 각 leadTime이 잘 생성 되는지 확인 
     def showLeadTime(self):
         for i in range(0, self.operatingNum):
             print('-------------------')
             print(f"{i +1}번째 machine lead time : ", self.leadTimes[i])
-            
+
+##각 단계에서 사용하는 machine에 맞는 leadtime을 가져온다.
+    def getLeadTime(self, machineNum: int) -> int:
+        leadTime = self.leadTimes[machineNum]
+
+        return leadTime
         
 
 
@@ -35,13 +42,36 @@ class Job():
 class Machine():
     def __init__(
             self,
-            leadTime: int| float = 1,   ##given by Job
-            Mode: bool = False, ##False means permutation / True means nonPermutation
+            job: Job = None,
+            machineNum: int = 0,
+            leadTime: int| float = 0,   ##given by Job
+            mode: bool = False, ##False means permutation / True means nonPermutation
             inspection: bool = False,   ##True면 해당 machine은 inspection을 담당하는 machine.
-            available: bool = True,     ##True면 해당 machine은 현재 job을 받을 수 있는 상태
+            available: bool = True,     ##True면 해당 machine은 현재 job을 받을 수 있는 상태 
     ):
-        self.leadTime: int| float = leadTime
-        self.inspection: bool = inspection
+    ##set attributes 
+        self.job = job
+        self.machineNum = machineNum
+        self.leadTime = leadTime
+        self.mode = mode
+        self.inspection = inspection
+        self.available = available
+    
+
+##Job을 할당할때 setMachine을 이용해서 Machine을 세팅한다.
+    def setMachine(
+            self,
+            job: Job,
+            leadTime: int,
+    ):
+        self.job = job
+        self.leadTime = job.getLeadtime(machineNum = self.machineNum)
+        self.available = False
+    
+    
+    
+
+
 
 
 class Environment():
@@ -51,23 +81,42 @@ class Environment():
             machineNum: int = 1, ##해당 갯수만큼의 Machine List를 만듦
             jobNum: int = 5 ##해당 갯수만큼의 Job List를 만듦.
     ):
-        self.Mode = Mode
-        self.machineNum = machineNum
-        self.makespan=0 ##전체 Process소요시간
+        self.Mode: bool = Mode
+        self.machineNum: int = machineNum
+        self.makespan:int = 0  ##전체 Process소요시간
+        self.jobPool: List[Job]
 
+
+
+
+
+"""
+machine갱신 logic:
+    전체 시간 -1
+    makespan += 1
+    for i = 0에서 마지막까지
+    현재 남은 시간 체크 
+    현재 남은시간 == 0:
+        현재 machine상태 = available
+        남은 시간 = 0인 machine들 체크 
+    
+    for i in 남은시간=0인machine:
+        다음 Machine == available인지 확인
+"""
 
 
 
 ## ---------  Test Area ------------------------------------------------------------------------------------------------------------------------------
 def main():
 ## Creat Job Process
-    jobTest = Job()
+    job = Job()
 
-##Show Created Jobs
-    jobTest.showLeadTime()
-        
-
-
+## Test job method
+    for i in range(0,10):
+        leadTime = job.getLeadTime(i)
+        print('-------------------')
+        print(f"{i}번째 machine lead time : ", leadTime)
+    
 
 
 
